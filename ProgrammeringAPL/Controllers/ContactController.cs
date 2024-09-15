4133f6b510afbf215e77341794ad84d6e89b1282
Andreas W <andreas.wickerfalt@gmail.com>
2024-09-15 20:44:55 +0200

    Lade till kontakt-sidan och ändrade stil på hela sidan

---
+++ ProgrammeringAPL/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammeringAPL.Models;
+
+namespace ProgrammeringAPL.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new ContactFormModel());
+        }
+        [HttpPost]
+        public IActionResult Submit(ContactFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Hantera inlämningen, t.ex. skicka e-post eller spara till en databas
+                ViewBag.Message = "Formuläret skickades korrekt!";
+                return View("Success"); // Laddar en vy som bekräftar inlämningen
+            }
+
+            // Om något är felaktigt, skicka tillbaka formuläret med felmeddelanden
+            return View("Index", model);
+        }
+    }
+}
---
+++ ProgrammeringAPL/Models/ContactFormModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammeringAPL.Models
+{
+    
+
+    public class ContactFormModel
+    {
+        [Required(ErrorMessage = "Namn krävs.")]
+        [MinLength(2, ErrorMessage = "Ditt namn måste vara minst 2 tecken långt.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "E-post krävs.")]
+        [EmailAddress(ErrorMessage = "Ange en giltig e-postadress.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Ett meddelande krävs.")]
+        [MinLength(10, ErrorMessage = "Meddelandet måste vara minst 10 tecken långt.")]
+        public string Message { get; set; }
+    }
+
+}
